5bb2ea28089846948de39b78a1a284ebe142ac17
benjamimaogoncalves-gif <benjamimaogoncalves@gmail.com>
2026-01-17 17:02:37 -0300

    Refactor image loading in app.py
    Removed unnecessary comment and adjusted image loading.

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ gray_500 = "#757575"
 # 1.B. LAYOUT
 # ajustar a configuração padrão da página | configure the default settings of the page
 
-st.image(Path, or list of these)
 itau_logo_completo_laranja = st.image(Path="https://i0.wp.com/assets.b9.com.br/wp-content/uploads/2023/12/itau-nova-marca.png?fit=1920%2C1080&ssl=1")
 itau_logo_laranja = st.image(Path="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Ita%C3%BA_Unibanco_logo_2023.svg/500px-Ita%C3%BA_Unibanco_logo_2023.svg.png")
 itau_home = "https://www.itau.com.br/"

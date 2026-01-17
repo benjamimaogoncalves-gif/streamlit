423794f95f826fa5f04e704451351249d15b4f55
benjamimaogoncalves-gif <benjamimaogoncalves@gmail.com>
2026-01-17 18:03:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,9 +21,9 @@ gray_500 = "#757575"
 # 1.B. LAYOUT
 # ajustar a configuração padrão da página | configure the default settings of the page
 
-itau_logo_completo_laranja = st.image(Path="https://i0.wp.com/assets.b9.com.br/wp-content/uploads/2023/12/itau-nova-marca.png?fit=1920%2C1080&ssl=1")
-itau_logo_laranja = st.image(Path="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Ita%C3%BA_Unibanco_logo_2023.svg/500px-Ita%C3%BA_Unibanco_logo_2023.svg.png")
-itau_home = "https://www.itau.com.br/"
+# itau_logo_completo_laranja = st.image(Path="https://i0.wp.com/assets.b9.com.br/wp-content/uploads/2023/12/itau-nova-marca.png?fit=1920%2C1080&ssl=1")
+# itau_logo_laranja = st.image(Path="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Ita%C3%BA_Unibanco_logo_2023.svg/500px-Ita%C3%BA_Unibanco_logo_2023.svg.png")
+# itau_home = "https://www.itau.com.br/"
 
 st.set_page_config(
     page_title="Calculadora de Rentabilidade",
